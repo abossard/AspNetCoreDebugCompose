f4e36999a7bd44e34b90cad41190e9bd1d8381ca
Andre Bossard <abossard@gmail.com>
2018-06-22 17:10:41 +0200

    step 1

--- Startup.cs
+++ Startup.cs
@@ -39,9 +39,14 @@ namespace app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            const string connection = @"Server=db;Database=master;User=sa;Password=Your_password123;";
+
+            
             // Add framework services.
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(connection));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
